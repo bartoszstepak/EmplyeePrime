b85eb6941c0684707c52ccd556f1e942ea06f606
bartoszstepak <46901072+bartoszstepak@users.noreply.github.com>
2020-03-05 16:44:55 +0100

    Add files via upload

---
+++ MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using crud_2.Models;
+
+namespace crud_2
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile() {
+            CreateMap<Employee, EmployeeDTO>().ReverseMap();
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+using AutoMapper;
+using crud_2.Controllers.BLL;
+using crud_2.Models;
+using crud_2.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Microsoft.IdentityModel.Tokens;
+
+namespace crud_2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddCors(o => 
+                o.AddPolicy("MyPolicy", builder =>
+               builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+                )
+            );
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+
+                        ValidIssuer = "http://localhost:49362",
+                        ValidAudience = "http://localhost:4200",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"))
+                    };
+                });
+
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AllowNullCollections = true;
+                mc.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            services.AddTransient<IEmployeeBLL, EmployeeBLL>();
+            services.AddTransient<ILoginBLL, LoginBLL>();
+            services.AddTransient<IMyTaskBLL, MyTaskBLL>();
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<EmployeeContext>((options) => {
+                options.UseSqlServer(Configuration.GetConnectionString("DevConnectionLocal"));
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+            app.UseCors("MyPolicy");
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
